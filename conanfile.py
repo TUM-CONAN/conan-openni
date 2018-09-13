a1af60398dad72d22fd8c5355756eefac7070f57
julien.waechter <julien.waechter@gmail.com>
2018-09-13 16:18:31 +0200

    feat(conanfile): add conan script to build openni 2.2.0-rev-958951f

---
+++ conanfile.py
@@ -0,0 +1,118 @@
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment, MSBuild
+from conans.util import files
+import os
+import shutil
+
+class LibOpenniConan(ConanFile):
+    name = "openni"
+    version = "2.2.0-rev-958951f"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    exports = ["patches/FindOpenNI2.cmake"]
+    url = "https://gitlab.lan.local/conan/conan-openni"
+    license="Apache License 2.0"
+    description = "Open Natural Interaction."
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+    short_paths = False
+
+    def system_requirements(self):
+        if tools.os_info.linux_distro == "ubuntu":
+            pack_names = [
+                "libusb-1.0-0-dev",
+                "libudev-dev"
+            ]
+            installer = tools.SystemPackageTool()
+            installer.update()
+            installer.install(" ".join(pack_names))
+    
+    def source(self):
+        rev = "958951f7a6c03c36915e9caf5084b15ecb301d2e"
+        tools.get("https://github.com/fw4spl-org/OpenNI2/archive/{0}.tar.gz".format(rev))
+        os.rename("OpenNI2-" + rev, self.source_subfolder)
+
+    def build(self):
+        openni_source_dir = os.path.join(self.source_folder, self.source_subfolder)
+
+        if tools.os_info.is_windows:
+            msbuild = MSBuild(self)
+            openni_sln = os.path.join(openni_source_dir, "OpenNI.sln")
+            msbuild.build(project_file=openni_sln, targets=["OpenNI", "Devices\PS1080", "Devices\ORBBEC"], build_type=self.settings.build_type)
+        else:
+            autotools = AutoToolsBuildEnvironment(self)
+            build_args = [
+                'CFG={0}'.format(self.settings.build_type),
+                'ALLOW_WARNINGS=1',
+                'GLUT_SUPPORTED=0',
+                'INSTALL_LIB_DIR={0}'.format(os.path.join(self.package_folder, "lib")),
+                'INSTALL_INCLUDE_DIR={0}'.format(os.path.join(self.package_folder, "include"))
+            ]
+            autotools.make(args=build_args, target="main")
+            autotools.install()
+
+    def package(self):
+        self.copy("FindOpenNI2.cmake", src="patches", dst=".", keep_path=False)
+        self.copy(pattern="*", 
+                    dst="include/openni2",
+                    src="{0}/Include".format(self.source_subfolder),
+                    keep_path=True)
+        if tools.os_info.is_windows:
+            self.copy(pattern="*.dll", 
+                      dst="bin/openni2/Drivers/",
+                      src="{0}/Bin/x64-{1}/OpenNI2/Drivers/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.dll", 
+                      dst="bin/openni2",
+                      src="{0}/Bin/x64-{1}/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.lib", 
+                      dst="lib/openni2",
+                      src="{0}/Bin/x64-{1}/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.lib", 
+                      dst="lib/openni2/Drivers/",
+                      src="{0}/Bin/x64-{1}/OpenNI2/Drivers/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*", 
+                      dst="bin/openni2",
+                      src="{0}/Config".format(self.source_subfolder),
+                      keep_path=True)
+        else:
+            self.copy(pattern="*.dylib", 
+                      dst="lib/openni2/Drivers/",
+                      src="{0}/Bin/x64-{1}/OpenNI2/Drivers/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.dylib", 
+                      dst="lib/openni2",
+                      src="{0}/Bin/x64-{1}/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.so", 
+                      dst="lib/openni2/Drivers/",
+                      src="{0}/Bin/x64-{1}/OpenNI2/Drivers/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*.so", 
+                      dst="lib/openni2",
+                      src="{0}/Bin/x64-{1}/".format(self.source_subfolder, self.settings.build_type),
+                      keep_path=False)
+            self.copy(pattern="*", 
+                      dst="lib/openni2",
+                      src="{0}/Config".format(self.source_subfolder),
+                      keep_path=True)
+
+        if tools.os_info.is_linux:
+            # LINUX WARNING
+            # primesense-usb.rules should be copied to '/etc/udev/rules.d/557-primesense-usb.rules' with admin rights (sudo)
+            # orbbec-usb.rules should be copied to '/etc/udev/rules.d/558-orbbec-usb.rules' with admin rights (sudo)
+            self.copy(pattern="primesense-usb.rules", 
+                      dst="rules",
+                      src="{0}/Packaging/Linux/".format(self.source_subfolder),
+                      keep_path=False)
+            self.copy(pattern="orbbec-usb.rules", 
+                      dst="rules",
+                      src="{0}/Packaging/Linux/".format(self.source_subfolder),
+                      keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
